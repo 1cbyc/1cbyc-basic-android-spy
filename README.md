e212e37226a497f4b7f9fa1edf029e44dd8b5acc
1cbyc <isaacnsisong@gmail.com>
2023-04-08 00:23:37 +0100

    let's start with the web manager

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 1cbyc-basic-android-spy
+
+I would work on this through the weekend and next week, making sure everything runs properly. It'll be open-source for anyone who wants help maintain afterwards.
---
+++ includes/databaseGateway.js
@@ -0,0 +1,3 @@
+// not yet time to add details, I would determine DB structure soon (not tonight abeg)
+// Funny thing is, I am thinking of using a web interface to manage the data that'll be received from the user's android device.
+// Maybe I should even design that interface first, and work the main code later. Let me start work on assets/webpublic then. See ya!
